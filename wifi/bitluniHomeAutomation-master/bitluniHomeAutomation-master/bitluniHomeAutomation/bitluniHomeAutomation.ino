9d89877235bea3488a2c86e5ada5bdc7c63a62b1
Mateusz <118119847+SoluchM@users.noreply.github.com>
2023-07-01 20:50:52 +0200

    Update bitluniHomeAutomation.ino

--- wifi/bitluniHomeAutomation-master/bitluniHomeAutomation-master/bitluniHomeAutomation/bitluniHomeAutomation.ino
+++ wifi/bitluniHomeAutomation-master/bitluniHomeAutomation-master/bitluniHomeAutomation/bitluniHomeAutomation.ino
@@ -23,8 +23,8 @@ I'll be pleased if you'd do it by sharing http://youtube.com/bitlunislab
 #include "WaveFunction.h"
 #include "RF.h"
 
-const char* ssid = "UPC3846636";
-const char* password = "pv8uxkrk3Wch";
+const char* ssid = "******";
+const char* password = "********";
 
 ESP8266WebServer server(80);
 
